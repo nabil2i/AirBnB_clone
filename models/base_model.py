a13f0016b0e5f3cbb4ae6a6aa56b7ca30e5bf425
Nabil <nabil.noble01@gmail.com>
2022-10-17 06:58:54 +0000

    Edit

--- models/base_model.py
+++ models/base_model.py
@@ -15,16 +15,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
+        #task4
         if kwargs:
             for key, value in kwargs.items():
+                #do not add __class__
                 if key != "__class__":
                     setattr(self, key, value)
+            #convert created_at and updated_at to datetime object
             if hasattr(self, "created_at") and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             if hasattr(self, "updated_at") and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
-            #have unique id for each BaseModel
+            #task 3: have unique id for each BaseModel
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
